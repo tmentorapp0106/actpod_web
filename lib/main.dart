9b0bf4591a09275aadf7dc9b2d21bfea9b8850a8
Eason <eason91367@gmail.com>
2026-06-17 17:23:05 +0800

    fix login

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
-import 'package:actpod_web/api_manager/user_api_manager.dart';
-import 'package:actpod_web/api_manager/user_dto/create_login.dart';
-import 'package:actpod_web/api_manager/user_dto/get_user_info.dart';
 import 'package:actpod_web/design_system/color.dart';
-import 'package:actpod_web/features/player_page/player_screen.dart';
 import 'package:actpod_web/local_storage/user_info.dart';
 import 'package:actpod_web/router.dart';
+import 'package:actpod_web/services/auth_service.dart';
 import 'package:actpod_web/services/env_service.dart';
 import 'package:actpod_web/utils/cookie_utils.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -16,17 +12,16 @@ import 'package:go_router/go_router.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
 
 Future<void> main() async {
-  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(
-    options: const FirebaseOptions(
-      apiKey: "AIzaSyC98t3s2itcyGLZW1CaZhl_HhblEwwOZBk",
-      authDomain: "backstage.actpodapp.com", // only for web
-      projectId: "share-voice-77cc4",
-      appId: "1:633262239415:web:a015a02dbabf75c523e732",
-      messagingSenderId: "633262239415",
-    )
-  );
+      options: const FirebaseOptions(
+    apiKey: "AIzaSyC98t3s2itcyGLZW1CaZhl_HhblEwwOZBk",
+    authDomain: "backstage.actpodapp.com", // only for web
+    projectId: "share-voice-77cc4",
+    appId: "1:633262239415:web:a015a02dbabf75c523e732",
+    messagingSenderId: "633262239415",
+  ));
 
   await EnvService.load();
   setUrlStrategy(PathUrlStrategy());
@@ -34,27 +29,14 @@ Future<void> main() async {
 
   UserPrefs.cleanUser();
   CookieUtils.deleteCookie("userToken");
-  
-  await FirebaseAuth.instance.setPersistence(Persistence.LOCAL);
-  await FirebaseAuth.instance.authStateChanges().first;
-  User? firebaseUser = FirebaseAuth.instance.currentUser;
-  if (firebaseUser != null) {
-    CreateNewUserRes res = await userApiManager.thirdPartyCreateUserOrLogin(
-      await firebaseUser.getIdToken(), 
-      firebaseUser.email, 
-      firebaseUser.displayName,
-    );
-    if(res.code != "0000") {
-    } else {
-      CookieUtils.setCookie("userToken", res.data?.userToken?? "", expires: DateTime.now().add(const Duration(hours: 6)));
-    }
 
-    GetUserInfoRes userInfoRes = await userApiManager.getUserInfo();
-    if(userInfoRes.code != "0000") {
-    } else {
-      UserPrefs.setUserInfo(userInfoRes.userInfo);
-    }
+  try {
+    await AuthService.restoreFirebaseLogin();
+  } catch (_) {
+    UserPrefs.cleanUser();
+    CookieUtils.deleteCookie("userToken");
   }
+
   runApp(const ProviderScope(child: MyApp()));
 }
 
@@ -65,24 +47,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: const Size(375, 812),
-      builder: (BuildContext context, Widget? child) {
-        return MaterialApp.router(
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(
-            colorScheme: ColorScheme.fromSeed(
-              seedColor: Colors.white,
-              primary: DesignColor.primary50
+        designSize: const Size(375, 812),
+        builder: (BuildContext context, Widget? child) {
+          return MaterialApp.router(
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              colorScheme: ColorScheme.fromSeed(
+                  seedColor: Colors.white, primary: DesignColor.primary50),
             ),
-          ),
-          title: 'ActPod',
-          color: Colors.white,
-          // routerConfig: myRouter,
-          routerDelegate: myRouter.routerDelegate,
-          routeInformationParser: myRouter.routeInformationParser,
-          routeInformationProvider: myRouter.routeInformationProvider,
-        );
-      }
-    );
+            title: 'ActPod',
+            color: Colors.white,
+            // routerConfig: myRouter,
+            routerDelegate: myRouter.routerDelegate,
+            routeInformationParser: myRouter.routeInformationParser,
+            routeInformationProvider: myRouter.routeInformationProvider,
+          );
+        });
   }
 }
