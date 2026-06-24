0e98da060f8e2700f74b003f28a7ee191c637e12
Eason <eason91367@gmail.com>
2026-06-24 11:55:31 +0800

    add package count

--- lib/features/package_detail_page/providers.dart
+++ lib/features/package_detail_page/providers.dart
@@ -1,4 +1,5 @@
 import 'package:actpod_web/dto/package_dto.dart';
+import 'package:actpod_web/services/package_purchase_count_service.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final packageDetailProvider =
@@ -7,4 +8,9 @@ final packageDetailLoadingProvider =
     StateProvider.autoDispose<bool>((ref) => false);
 final packageDetailErrorProvider =
     StateProvider.autoDispose<String?>((ref) => null);
-final packagePurchasedProvider = StateProvider.autoDispose<bool?>((ref) => null);
+final packagePurchasedProvider =
+    StateProvider.autoDispose<bool?>((ref) => null);
+final packagePurchaseCountProvider =
+    FutureProvider.autoDispose.family<int, String>((ref, packageId) {
+  return PackagePurchaseCountService().getPurchaseCount(packageId);
+});
